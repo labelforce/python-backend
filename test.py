b7b5f4620a9a6d31eae630abdecb8b21929e7a20
Tim Dettmers <timdettmers@Tims-MacBook-Pro-2.local>
2015-10-04 04:35:20 +0200

    Added tsne visu of the codes.

--- test.py
+++
@@ -1,8 +0,0 @@
-
-
-from firebase import Firebase
-
-f = Firebase('https://boiling-heat-2521.firebaseio.com/', auth_token="eoyakpIFmf4LTm6JcUPElixc8ieeQujvDF7bCGNh")
-
-
-r = f.push({'user_id': 'wilma', 'text': 'Hello'})
